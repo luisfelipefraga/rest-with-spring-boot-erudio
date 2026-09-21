1c95b489fc9c961783613fb458967a627d42edc7
Luis Fraga <luisfelipersx@hotmail.com>
2026-09-21 12:51:40 -0300

    Primeiros passos com Spring

---
+++ rest-with-spring-boot-erudio/src/main/java/br/com/luisfelipefraga/rest_with_spring_boot_erudio/model/Greeting.java
@@ -0,0 +1,3 @@
+package br.com.luisfelipefraga.rest_with_spring_boot_erudio.model;
+
+public record Greeting(long id, String content) {}
---
+++ rest-with-spring-boot-erudio/src/test/java/br/com/luisfelipefraga/rest_with_spring_boot_erudio/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.luisfelipefraga.rest_with_spring_boot_erudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
